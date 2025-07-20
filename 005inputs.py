e7a83a63ff9d93bf10272125ac1c19d986276e6e
Ali Muhammad <alimuhammad5921@gmail.com>
2025-07-20 11:52:58 +0500

    inputs

---
+++ 005inputs.py
@@ -0,0 +1,17 @@
+# a = input("Enter your name : ")
+# print("\nWelcome, ",a)
+
+# x = int(input("\nEnter first number: "))
+# y = int(input("Enter second number: "))
+
+# print("\nThe result of" , x , "+", y , "is :" , x+y)
+
+x1 = input("Enter first number : ")
+y1 = input("Enter second number : ")
+
+print(x1 + y1)
+print(int(x1) - int(y1))
+print(int(x1) * int(y1))
+print(int(x1) / int(y1))
+print(int(x1) // int(y1))
+print(int(x1) % int(y1))
