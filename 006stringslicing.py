850daa10e470b5c4a1bec6cc31c5fa7f88948b62
Ali Muhammad <alimuhammad5921@gmail.com>
2025-07-21 14:42:35 +0500

    string slicing

---
+++ 006stringslicing.py
@@ -0,0 +1,18 @@
+msg = 'You\'re a wizard , Harry '
+# for character in msg:
+#     print(character).
+
+print(len(msg))
+
+print(msg[0:5])  #includng 0 but not 5
+
+print(msg[10:15])   #includng 10 but not 15
+print(msg[:10])        #from 0 till 9
+print(msg[0:-5])    #from 0 till length of msg - 5
+print(msg[:len(msg)-5])
+print(msg[-1:len(msg)-5])
+print(msg[-10:-1])
+
+nm = "Harry"
+print(nm[-4:-2])
+
