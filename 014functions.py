9e98217b1e5b706927a0a3dbaaadf6e0daa73ede
Ali Muhammad <alimuhammad5921@gmail.com>
2025-07-22 09:55:43 +0500

    user defined functions

---
+++ 014functions.py
@@ -0,0 +1,15 @@
+def calGmean(a ,b):
+    mean = (a*b) / (a+b)
+    print(mean)
+
+a = 10
+b = 5
+calGmean(a,b)
+
+def isGreater(a , b):
+    if(a > b):
+        print("A is greater than B ")
+    else : 
+         print("B is greater than A")
+
+isGreater(a , b)
