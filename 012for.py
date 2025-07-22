eb2e3ef4c50baa05168fbd66660093d5428a6d50
Ali Muhammad <alimuhammad5921@gmail.com>
2025-07-22 09:25:28 +0500

    for loop

---
+++ 012for.py
@@ -0,0 +1,16 @@
+# name = "Harry Potter"
+# for i in name :
+#     print(i)
+
+names = ["Harry Potter","Ron Weasley ","Hermoine Granger"]
+for name in names:
+    print(name)
+
+for i in range(5): # 5 nhi print hoga
+    print(i)
+
+for k in range(1,10):
+    print(k)
+
+for a in range(1,15,3): # 3 humara step ka argument hy
+    print(a)
