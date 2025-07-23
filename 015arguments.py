3f980ee152228aacdcfddeb65dc4232e0b77b71b
Ali Muhammad <alimuhammad5921@gmail.com>
2025-07-23 09:50:47 +0500

    arguments of functions

---
+++ 015arguments.py
@@ -0,0 +1,18 @@
+# def average(a = 10 , b = 5):
+#     print("The average of ",a ,"and", b , "ïs : ", (a+b)/2)
+
+# average() # agr average nhi daingy to default arguments use hongy
+# average(4)
+# average(4 ,5)
+# average(b = 10 , a = 8) # keywords arguments
+
+def average(*numbers):
+    print(type(numbers))
+    sum = 0
+    for i in numbers:
+        sum = sum+i
+    #print("The average of all numbers is : ", sum/len(numbers))
+    return sum/len(numbers)
+
+avg = average(5,6)
+print(avg)
