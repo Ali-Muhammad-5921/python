fc9bf978d11fac966166380c5d9d753b60f63441
Ali Muhammad <alimuhammad5921@gmail.com>
2025-07-25 08:41:51 +0500

    sets in python

---
+++ 024sets.py
@@ -0,0 +1,14 @@
+# sets is a collection of non - repeating and unordered objects
+
+s = {2,4,2,6}
+print(s)
+s1 = {True , 20 ,False , "Caroline",10} # lazmi nhi k isi order ma print hongy
+print(s1)
+#sets ki values chnage nhi ho skti 
+
+#how to make an empty set
+set1 = set()
+print(type(set1))
+
+# and not like this q k yeh dictionary bnata hy
+# set1 = {}
