ed3f4023387acf37bc1980bda6dbe841fe01a9d9
Ali Muhammad <alimuhammad5921@gmail.com>
2025-07-25 09:26:40 +0500

    else with loops

---
+++ 027forElse.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    print(i)
+# else sirf tb execute hoga jb loop successfully complete hua hy na  k break hua hy
+# agr hum break  condition ki wajah sy loop sy bahir ayn to else nhi execute hoga 
+else :
+    print('sorry no 5')
+
+# same with while loop 
