8b8068f0aef8108ef330723e3fbc5e1ccb7d8aea
Ali Muhammad <alimuhammad5921@gmail.com>
2025-07-26 08:19:51 +0500

    exception handling

---
+++ 028eceptionHandling.py
@@ -0,0 +1,11 @@
+# try or catch sy exception handling hoti hy python ma 
+
+
+try :
+    a = int(input("Enter a number : "))
+    print(f'The square of {a} is : {a*a}')
+except Exception as e :
+    print(e) # idht e ki jagha apni marzi ka koi msg bhi likh skty hain
+
+print("some important lines of code .")
+print("End of code .")
