88685d77247b5f2bb523e18b1ea75a4017e0b7e4
Ali Muhammad <alimuhammad5921@gmail.com>
2025-07-27 09:40:58 +0500

    virtual env

---
+++ 034virtualEnvironment.py
@@ -0,0 +1,5 @@
+# Day 43 of 100 days of code is the video about how to create virtual environment
+
+# command for creating virtual environment is : 
+# pyhton3 -m venv (environement name ) 
+# for eg : python3 -m venv abc
