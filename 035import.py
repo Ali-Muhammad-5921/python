162ae6dfd03cd31ec3e209118f92faee16e794f7
Ali Muhammad <alimuhammad5921@gmail.com>
2025-07-27 09:55:44 +0500

    how import works

---
+++ 035import.py
@@ -0,0 +1,8 @@
+import math
+# import math as m prh m. or sary functions 
+# from math import sqrt as s to s() sy hum function access kr skty hain
+# from math import sqrt yeh tb use krty hain jb koi aik function use krna hy is case ma sirf sqrt import hoga
+# print(dir(math)) yeh math k sary functions display kry ga
+
+a = math.sqrt(9)
+print(a)
