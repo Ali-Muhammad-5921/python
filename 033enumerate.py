9672ea4d6fcc6096759584e5295a3ac9606a04a8
Ali Muhammad <alimuhammad5921@gmail.com>
2025-07-27 09:28:47 +0500

    enumerate function

---
+++ 033enumerate.py
@@ -0,0 +1,7 @@
+marks = [78 ,19, 87 ,12, 56 , 43]
+
+#for a in enumerate(marks): yeh aik tuple de ga 
+for index , mark in enumerate(marks):
+    print(mark)
+    if index == 3:
+        print("gio") 
