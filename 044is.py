c45a95ce367afbd6aaece9809757100a1f30bfc6
Ali Muhammad <alimuhammad5921@gmail.com>
2025-07-30 07:26:14 +0500

    is and ==

---
+++ 044is.py
@@ -0,0 +1,13 @@
+# is and = dono hi comparison operator hoty hain 
+# = compares exaclty values 
+# is compares the exact location
+
+# a = 4
+# b = 4
+# print(a is b)   output = True 
+# print(a == b)   output = True
+
+a = [1,2,3]
+b = [1,2,3]
+print(a is b) # False
+print(a == b) # True
