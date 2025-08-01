071e114010c46646ea790f95f9fd07196765c077
Ali Muhammad <alimuhammad5921@gmail.com>
2025-08-02 02:17:24 +0500

    static Methods

--- 053StaticMethods.py
+++ 053StaticMethods.py
@@ -9,3 +9,4 @@ class Math:
 a = Math()
 
 print(a.add(10 , 25))# as add method does'nt return anything so none is printed
+# hum static method ko normal method ki trah use krty hain or isko class name sy or instance name sy access krty hain
