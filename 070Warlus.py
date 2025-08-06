8f6dfe229108cdb326cb339d4cd252fb29231f76
Ali Muhammad <alimuhammad5921@gmail.com>
2025-08-06 17:49:15 +0500

    Shutil

--- 070Warlus.py
+++ 070Warlus.py
@@ -6,3 +6,7 @@ while (n := len(nums)) > 0:
     print(nums.pop())
 
 # assigns value as a part of larger expression
+
+foods = list()
+while (food := input('Enter food you like:')) != 'quit':
+    foods.append(food)
---
+++ 071Shutil.py
@@ -0,0 +1,7 @@
+import shutil
+
+shutil.copy('071Shutil.py','shutilcp.py') 
+# this creates a copy of the file 
+# copy2 is similar but preserves a bit more meta data such as timestamp
+# copytree can copy entire folder
+# similarly there are many other functions as well like move to move locations of files , rmtree can delter folders
