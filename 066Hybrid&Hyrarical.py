0f3ecf280ceb34aae15abca7d1249230e8518f1a
Ali Muhammad <alimuhammad5921@gmail.com>
2025-08-06 09:33:10 +0500

    Hybrid and Hierarchical Inheritence

---
+++ 066Hybrid&Hyrarical.py
@@ -0,0 +1,17 @@
+# HYbrid Inheritence using different types of inheritence
+
+class BaseClass:
+    pass
+
+class Derived1(BaseClass):
+    pass
+
+class Derived2(BaseClass):
+    pass
+
+class Derived3(Derived1,Derived2):
+    pass
+
+# Hierarchical Inheritence
+
+# It has a hierarchical structure like any origanization or in simple words a tree like
